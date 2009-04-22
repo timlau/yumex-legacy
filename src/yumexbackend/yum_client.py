83243b3c4def73c666e389367a3364c761751cdb
Tim Lauridsen <timlau@fedoraproject.org>
2009-04-22 14:40:43 +0200

    renamed yum_server.py to yum_childtask.py

--- src/yumexbackend/yum_client.py
+++ src/yumexbackend/yum_client.py
@@ -184,9 +184,9 @@ class YumClient:
         if not self.child:
             if repos:
                 repo_str = ";".join(repos)
-                self.child = pexpect.spawn(MAIN_PATH+'/yum_server.py %i %s %s' % (debuglevel,plugins,repo_str), timeout=self._timeout_value)
+                self.child = pexpect.spawn(MAIN_PATH+'/yum_childtask.py %i %s %s' % (debuglevel,plugins,repo_str), timeout=self._timeout_value)
             else:    
-                self.child = pexpect.spawn(MAIN_PATH+'/yum_server.py %i %s' % (debuglevel,plugins),timeout=self._timeout_value)
+                self.child = pexpect.spawn(MAIN_PATH+'/yum_childtask.py %i %s' % (debuglevel,plugins),timeout=self._timeout_value)
             self.child.setecho(False)
             if filelog:
                 self.child.logfile_read = sys.stdout
