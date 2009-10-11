1117c3586e891483425cabf483a2df6d6b015538
Tim Lauridsen <timlau@fedoraproject.org>
2009-10-11 08:44:22 +0200

    fix missing self (rhbz #528302)

--- src/yumexbackend/yum_server.py
+++ src/yumexbackend/yum_server.py
@@ -176,7 +176,7 @@ class YumServer(yum.YumBase):
         '''
         Check if an repo is local (media or file:// repo)
         '''
-        if repo.mediaid or _is_fileurl(repo.baseurl):
+        if repo.mediaid or self._is_fileurl(repo.baseurl):
             return True
         else:
             return False
