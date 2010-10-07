7a68a4b3c3ced8932852c1200a6c83cb548d6664
Tim Lauridsen <timlau@fedoraproject.org>
2010-10-07 14:53:52 +0200

    just some test code

--- src/poc/test-notify.py
+++ src/poc/test-notify.py
@@ -1,16 +1,30 @@
 #!/usr/bin/python -tt
 
 import pynotify
+import gtk
 
-def callback_function(notification=None, action=None, data=None):
-     print "It worked!"
-     print notification, action, data
+class YumexNotification(pynotify.Notification):
+    def __init__(self, title, body):
+        pynotify.init("Yum Extender")
+        pynotify.Notification.__init__(self, title, body)
+        self.set_urgency(pynotify.URGENCY_NORMAL)
+        self.set_timeout(pynotify.EXPIRES_NEVER)
 
-pynotify.init("Some Application or Title")
-n = pynotify.Notification("Title", "body", "dialog-info")
-n.set_urgency(pynotify.URGENCY_NORMAL)
-n.set_timeout(pynotify.EXPIRES_NEVER)
+    def add_button(self, title):
+        self.add_action("clicked", title, self.callback_function, title)
 
-n.add_action("clicked", "Open", callback_function, None)
-n.add_action("clicked", "Close", callback_function, None)
-n.show()
+    def callback_function(self, notification=None, action=None, data=None):
+         print "Notification Action: %s clicked" % data
+
+
+if __name__ == "__main__":
+    win = gtk.Window()
+    win.connect('delete-event', gtk.main_quit)
+    win.set_size_request(200, 50)
+    fancy_label = gtk.Label("Python Notification Test")
+    win.add(fancy_label)
+    win.show_all()
+    n = YumexNotification("Yum Extender", "Some information to the user")
+    n.add_button('Ok')
+    n.show()
+    gtk.main()
